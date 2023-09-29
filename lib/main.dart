8d3ef5df0906310c6240d67740e0e8ce92834b8a
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2023-09-29 11:10:13 +0100

    Added Material App, Scaffold and other widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dev/newgradient_container.dart';
 
+// void main() => runApp(
+//       const MaterialApp(
+//         home: Scaffold(
+//           body: NewGradientContainer(),
+//         ),
+//       ),
+//     );
+
 void main() => runApp(
-      const MaterialApp(
+      MaterialApp(
         home: Scaffold(
-          body: NewGradientContainer(),
+          body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.black, Colors.white],
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft),
+            ),
+            child: const Center(
+              child: Text(
+                'Hello World, this is my first Flutter App',
+                style: TextStyle(
+                  backgroundColor: Colors.blue,
+                  color: Colors.white,
+                  fontSize: 26.5,
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
