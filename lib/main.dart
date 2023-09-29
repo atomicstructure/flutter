9f612488cdbaf41fe16c02fcaa3d9bfaa200d6e0
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2023-09-29 12:05:07 +0100

    Broke my codes to functions

--- lib/main.dart
+++ lib/main.dart
@@ -10,26 +10,32 @@ import 'package:flutter_dev/newgradient_container.dart';
 //     );
 
 void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  colors: [Colors.black, Colors.white],
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft),
-            ),
-            child: const Center(
-              child: Text(
-                'Hello World, this is my first Flutter App',
-                style: TextStyle(
-                  backgroundColor: Colors.blue,
-                  color: Colors.white,
-                  fontSize: 26.5,
-                ),
-              ),
-            ),
+      const MaterialApp(
+        home: Scaffold(body: NewGradient()),
+      ),
+    );
+
+class NewGradient extends StatelessWidget {
+  const NewGradient({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            colors: [Colors.black, Colors.white],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World, this is my first Flutter App',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 26.5,
           ),
         ),
       ),
     );
+  }
+}
